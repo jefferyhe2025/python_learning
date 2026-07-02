f65a18a859621e6fc8a86f7e254ef52f3375bfe3
何冠臻 <3267044902@qq.com>
2026-07-02 08:21:13 +0800

    修改昨天练习的错误

--- practice2.py
+++ practice2.py
@@ -31,16 +31,14 @@ weight = float(input("请输入你的体重（kg）："))
 height = float(input("请输入你的身高（cm）："))
 bmi = weight / (height / 100) ** 2
 print(f'{bmi = :.1f}')
-if 18.5 <= bmi <= 25:
+if 18.5 < bmi <= 25:
     print('你的身材很曼妙😍')
-elif bmi < 18.5:
-    print('瘦成啥样了😱')
-elif bmi < 25:
+elif 25 < bmi <= 30:
     print('小心变成大胃袋🤦')
-elif bmi < 30:
-    print('你成为小胃袋🫢')
+elif 30 < bmi:
+    print('合格的大胃袋😇')
 else:
-    print('已经是合格大胃袋😇')
+    print('瘦成啥了')
 
 
 """
@@ -83,16 +81,15 @@ Version: 1.0
 """
 score = int(input("请输入你的成绩:"))
 if score >= 90:
-    grade = "A"
+    print('A')
 elif score >= 80:
-    grade = "B"
+    print('B')
 elif score >= 70:
-    grade = "C"
+    print('C')
 elif score >= 60:
-    grade = "D"
+    print('D')
 else:
-    grade = "F"
-print(f'{grade = }')
+    print('F')
 
 """
 计算三角形的周长和面积
@@ -103,10 +100,10 @@ a = int(input('a = '))
 b = int(input('b = '))
 c = int(input('c = '))
 if a + b > c and a + c > b and b + c > a:
-    perimeter = a + b + c
+    perimeter  = a + b + c
     s = perimeter / 2
-    area  = (s * (s - a) * (s - b) * (s - c)) ** 0.5
-    print(f'{perimeter = }')
-    print(f'{area = }')
+    area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
+    print(f'{perimeter = :.1f}')
+    print(f'{area = :.1f}')
 else:
-    print('三条边不能构成三角形')
+    print('三边不能构成三角形')
