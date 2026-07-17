fec85d5a5d65f5108b4335a77ffc4bfb9bb0e750
何冠臻 <jefferyhe2024@gmail.com>
2026-07-17 14:15:32 +0800

    修改扑克牌程序的一些错误

--- project/OOP_in_practice.py
+++ project/OOP_in_practice.py
@@ -10,7 +10,7 @@ from enum import Enum
 
 class Suite(Enum):
     """ 花色的文字符号 """
-    SPADE,HEART,CLUB,DIMOND = range(4) #用枚举的方式将文字符号替代数字🔢
+    SPADE,HEART,CLUB,DIAMOND = range(4) #用枚举的方式将文字符号替代数字🔢
 
 class Card:
     def __init__(self,suite,face):
@@ -33,8 +33,8 @@ class Card:
             return self.face < other.face
         return self.suite.value < other.suite.value
 
-class Poker:
-    """ 扑克 """
+class Decker:
+    """ 牌堆相关的功能 """
     def __init__(self):
         """ 创建52张牌，记录发牌位置"""
         self.cards = [Card(suite,face)
@@ -42,10 +42,10 @@ class Poker:
                       for face in range(1,14)]
         self.current = 0
 
-    def shuffle(self):
+    def shuffle(self) -> None:
         """ 洗牌 """
         self.current = 0
-        return random.shuffle(self.cards)
+        random.shuffle(self.cards) #返回值为None，不用return
 
     def deal(self):
         """ 发牌 """
@@ -64,22 +64,22 @@ class Player:
         self.name = name
         self.cards = []
 
-    def get_one(self,card):
+    def get_one(self,card) -> None:
         """ 摸牌 """
-        return self.cards.append(card)
+        self.cards.append(card) #返回值为None，不用return
 
-    def arrange_card(self):
+    def arrange_card(self) -> None:
         """ 理牌 """
-        return self.cards.sort()
+        self.cards.sort() #返回值为None，不用return
         #📝这里整理排序会对牌堆中Card类进行比大小，故而在Card类中添加了__lt__魔法方法
 
-poker = Poker()
-poker.shuffle()
+decker = Decker()
+decker.shuffle()
 players = [Player('张三'),Player('李四'),Player('王五'),Player('赵六')]
 #发牌
 for _ in range(13):
     for player in players:
-        player.get_one(poker.deal())
+        player.get_one(decker.deal())
 #玩家理牌
 for player in players:
     player.arrange_card()
@@ -111,7 +111,7 @@ class Manager(Employee):
 
 class Programmer(Employee):
     """ 程序员 """
-    def __int__(self,name,work_hours):
+    def __init__(self,name,work_hours):
         super().__init__(name)
         self.work_hours = work_hours
 
@@ -125,7 +125,7 @@ class Salesman(Employee):
         self.sales = sales
 
     def get_salary(self):
-        return 1800 + self.sales ** 0.05
+        return 1800 + self.sales * 0.05
 
 #创建员工实例
 emps = [Manager('刘备'), Programmer('诸葛亮'), Manager('曹操'), Programmer('荀彧'), Salesman('张辽')]
