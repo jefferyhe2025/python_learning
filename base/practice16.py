7e2a66ac1a4dca9e295b985e4f465052a27e6dbe
何冠臻 <jefferyhe2024@gmail.com>
2026-07-18 09:41:38 +0800

    增加若干注释

--- base/practice16.py
+++ base/practice16.py
@@ -4,20 +4,19 @@ import random
 将数据写入 CSV 文件
 """
 with open('file/scores.csv', 'w', encoding='utf-8') as file:
-    # 改变分隔符、包围方式
-    writer = csv.writer(file,delimiter='|',quoting=csv.QUOTE_ALL)
+    writer = csv.writer(file,delimiter='|',quoting=csv.QUOTE_ALL) # 改变分隔符、包围方式
     writer.writerow(['姓名', '语文', '数学', '英语'])
     names = ['张三','李四','王五','赵六']
     for name in names:
-        score = [random.randrange(60,100) for _ in range(3)]
-        score.insert(0,name)
-        writer.writerow(score)
+        scores = [random.randrange(60,100) for _ in range(3)]
+        scores.insert(0,name)
+        writer.writerow(scores)
 
 """
 读取csv文件的数据
 """
 with open('file/scores.csv','r',encoding='utf-8') as file:
-    reader = csv.reader(file,delimiter='|')
+    reader = csv.reader(file,delimiter='|')  # 默认去除引号包围方式
     for datalist in reader:
         #打印行数
         print(reader.line_num,end='\t')
