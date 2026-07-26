94600c206f499d06d796a1740fe23c375e7a9df6
何冠臻 <jefferyhe2024@gmail.com>
2026-07-26 10:58:42 +0800

    实现庄家规则 + 比点 + 赔率

--- project/blackjack/game/blackjack.py
+++ project/blackjack/game/blackjack.py
@@ -1,6 +1,15 @@
+from enum import Enum, auto
 from project.blackjack.models.deck import Deck
 from project.blackjack.models.player import Player, Dealer
 from project.blackjack.ui.console import ConsoleUI
+
+
+class Outcome(Enum):
+    """ 打印结果"""
+    LOSE = auto()
+    WIN = auto()
+    PUSH = auto()
+    BLACKJACK = auto()
 
 
 class BlackJackGame:
@@ -32,6 +41,25 @@ class BlackJackGame:
         self.player.receive_card(self.deck.deal())
         self.dealer.receive_card(self.deck.deal())
 
+    def _check_natural(self):
+        """ 开局检查是否有自然blackjack"""
+        p = self.player.hand.is_blackjack()
+        d = self.dealer.hand.is_blackjack()
+
+        if not p and not d:
+            # 回合继续
+            return False
+        if p and d:
+            # 平局
+            self._settle(Outcome.PUSH)
+        elif p:
+            # 玩家赢
+            self._settle(Outcome.BLACKJACK)
+        else:
+            # 庄家赢
+            self._settle(Outcome.LOSE)
+        return True # 结束
+
     def _player_turn(self):
         """ 玩家回合，True = 爆牌 """
         while True:
@@ -44,19 +72,59 @@ class BlackJackGame:
             if self.player.hand.is_bust:
                 return True
 
-    def _settle_player_bust(self):
-        print(f'\n你爆了，输掉：{self.bet},还剩筹码：{self.player.chips}')
+    def _dealer_turn(self):
+        """ 庄家回合 """
+        self.dealer.reveal()
+        while self.dealer.should_hit():
+            """ 点数不满17，一直hit"""
+            self.dealer.receive_card(self.deck.deal())
+
+    def _settle(self,outcome):
+        """ 结算bet """
+        bet = self.bet
+        if outcome is Outcome.LOSE:
+            pass #输了扣除bet
+        elif outcome is Outcome.WIN:
+            self.player.chips += bet * 2 # 退本 + 1:1
+        elif outcome is Outcome.PUSH:
+            self.player.chips += bet # 退本
+        elif outcome is Outcome.BLACKJACK:
+            self.player.chips += bet + bet * 3 // 2
+        else:
+            raise ValueError(outcome)
+
+        print(f"结果：{outcome.name}，本局注：{bet}，剩余筹码：{self.player.chips}")
 
     def play_round(self):
         """ 单局流程"""
         if self.deck.current == 0:
             # 第一局先洗牌
             self.deck.shuffle()
-        self._place_bet()
+        self._place_bet() # 下注
         self._ensure_deck()
-        self._deal_initial()
+        self._deal_initial() # 闲家、庄家各发两张牌
+        if self._check_natural():
+            return
         if self._player_turn():
-            self._settle_player_bust()
+            # 玩家回合
+            self._settle(Outcome.LOSE)
+            print('你爆了，扣除赌注！')
             return
         print('stand,庄家回合')
+        self._dealer_turn()
+        # 比点数
+        pv,dv = self.player.hand.best_value(),self.dealer.hand.best_value()
 
+        if self.dealer.hand.is_bust:
+            # 庄家爆牌，玩家胜利
+            outcome = Outcome.WIN
+        elif pv > dv:
+            # 玩家点数大，玩家获胜
+            outcome = Outcome.WIN
+        elif pv < dv:
+            # 庄家点数大，玩家输了
+            outcome = Outcome.LOSE
+        else:
+            # 点数相同，平局
+            outcome = Outcome.PUSH
+        self._settle(outcome) # 结算
