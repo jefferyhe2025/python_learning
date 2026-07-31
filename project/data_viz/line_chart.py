c7cdf4814bcbd64cb73b77f3c45e1207e2b8e3ad
何冠臻 <jefferyhe2024@gmail.com>
2026-07-31 17:30:56 +0800

    绘制平方折线图、立方折线图

--- project/data_viz/line_chart.py
+++ project/data_viz/line_chart.py
@@ -1,18 +1,19 @@
 import matplotlib.pyplot as plt
 
-x_values = range(1,1001)
-y_values= [x ** 2 for x in x_values]
+x_values = range(1, 5001)
+y_values= [x ** 3 for x in x_values]
 
 plt.style.use('seaborn-v0_8-talk')
 fg,ax = plt.subplots()
-ax.scatter(x_values,y_values,color=(0,0.8,0),s=10)
-ax.plot(x_values,y_values,linewidth = 3)
+ax.scatter(x_values,y_values,c=y_values,cmap='viridis',s=10)
+# ax.plot(x_values,y_values,linewidth = 3) # 绘制线条的粗细
 
-ax.set_title('Square Numbers',fontsize=24)
+ax.set_title('Cube Numbers',fontsize=24)
 ax.set_xlabel('Value',fontsize = 14)
-ax.set_ylabel('Square of Value',fontsize=14)
-ax.axis([0,1100,0,1_100_000])
+ax.set_ylabel('Cube of Value',fontsize=14)
+ax.axis([0,5000,0,125_000_000_000])
+# ax.ticklabel_format(style='plain')
 
 ax.tick_params(labelsize=14)
 
-plt.show()
+plt.savefig('cube_plt.png')
