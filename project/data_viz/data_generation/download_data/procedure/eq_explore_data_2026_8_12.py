9c0bd4c9e9cc3795184bea9967aac1ab0388fc8f
何冠臻 <jefferyhe2024@gmail.com>
2026-08-12 14:57:58 +0800

    最近30天地震散点图

---
+++ project/data_viz/data_generation/download_data/procedure/eq_explore_data_2026_8_12.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import json,plotly.express as px,pandas as pd
+
+# 读取数据
+path = Path('../eq_data/2026_8_12_eq_data_30_day_m1.geojson')
+contents = path.read_text()
+all_eq_data = json.loads(contents)
+
+# 提取地震信息
+all_eq_dict = all_eq_data['features']
+mags = [eq_dict['properties']['mag'] for eq_dict in all_eq_dict]
+titles = [eq_dict['properties']['title'] for eq_dict in all_eq_dict]
+lons = [eq_dict['geometry']['coordinates'][0] for eq_dict in all_eq_dict]
+lats = [eq_dict['geometry']['coordinates'][1] for eq_dict in all_eq_dict]
+
+data = pd.DataFrame(
+    data=zip(lons,lats,titles,mags),columns=['经度','纬度','位置','震级']
+)
+fig = px.scatter(
+    data,
+    x='经度',
+    y='纬度',
+    range_x=[-200,200],
+    range_y=[-90,90],
+    width=800,
+    height=800,
+    title=all_eq_data['metadata']['title'],
+    size='震级',
+    size_max=10,
+    color='震级',
+    hover_name='位置'
+)
+fig.write_html('2026_8_12_global_earthquakes.html')
