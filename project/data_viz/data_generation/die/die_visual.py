95f03cff99d811a6f544c8fa8c65a219da202bfe
何冠臻 <jefferyhe2024@gmail.com>
2026-08-19 14:50:16 +0800

    用value_counts方法统计点数出现的频率

--- project/data_viz/data_generation/die/die_visual.py
+++ project/data_viz/data_generation/die/die_visual.py
@@ -1,21 +1,27 @@
 import plotly.express as px
+import pandas as pd
 
 from die import Die
 
 # 创建骰子实例
-die_1 = Die(6)
+n_rolls = 50000
+die_1 = Die()
 die_2 = Die(10)
 # 投掷50,000次骰子并保存数据
-result = [die_1.roll() * die_2.roll() for roll_num in range(50_000)]
+result = [die_1.roll() * die_2.roll() for _ in range(n_rolls)]
 
-# 计算每个点数的出现的总次数
-poss_result = range(1,die_1.num_sides * die_2.num_sides +1)
-frequencies = [result.count(value) for value in poss_result]
+# 用value_counts统计所有出现数据的频率
+freq = pd.Series(result).value_counts().sort_index()
 
-title = 'Result of Rolling D6 and D10 50,000 Times' # 标题
+title = f'Result of Rolling D6 and D10 {n_rolls} Times' # 标题
 label = {'x':'Result','y':'Frequencies of Result'} # 坐标轴名称
-fig = px.bar(x=poss_result,y=frequencies,title=title,labels=label)
+fig = px.bar(x=freq.index,y=freq.values,title=title,labels=label)
 
-fig.update_layout(xaxis_dtick=1) # 柱形图下标
+fig.update_layout(
+    xaxis_dtick=1,
+    xaxis_tickangle=0,
+    xaxis_tickfont=dict(size=8)
+) # 柱形图下标
+
 
 fig.show(renderer="browser")
--- project/data_viz/data_generation/die/die_visual_02.py
+++ project/data_viz/data_generation/die/die_visual_02.py
@@ -1,23 +1,24 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from die import Die
 
 # 创建骰子实例
-die_1 = Die(6)
-die_2 = Die(6)
+n_rolls = 50000
+die_1 = Die()
+die_2 = Die()
 # 投掷50,000次骰子并保存数据
-result = [die_1.roll() + die_2.roll() for roll_num in range(50_000)]
+result = [die_1.roll() + die_2.roll() for _ in range(n_rolls)]
 
-# 计算每个点数的出现的总次数
-poss_result = range(2,die_1.num_sides + die_2.num_sides +1)
-frequencies = [result.count(value) for value in poss_result]
+# 用value_counts统计所有出现数据的频率
+freq = pd.Series(result).value_counts().sort_index()
 
 plt.figure(figsize=(12,6))
-plt.bar(poss_result,frequencies)
+plt.bar(freq.index,freq.values)
 
-plt.title('Result of Rolling Two D6 50,000 Times')# 标题
+plt.title(f'Result of Rolling Two D6 {n_rolls} Times')# 标题
 plt.xlabel('Result')
 plt.ylabel('Frequencies of Result')
-plt.xticks(poss_result)  # 每个整数刻度都显示
+plt.xticks(freq.index)  # 每个整数刻度都显示
 plt.tight_layout()
 plt.show()
